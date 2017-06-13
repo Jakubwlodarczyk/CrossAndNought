f93e0389521f1083958e9bbf23bb42d239225d62
Jakubwlodarczyk <Kubawl.91@gmail.com>
2017-06-13 18:59:24 +0200

    Delete outprints from Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,5 @@ public class Main {
     public static void main(String[] args) {
         Board board = new Board();
         board.init();
-        System.out.println(board);
-        System.out.println(board.getCells());
-        System.out.println(board.getCells()[0][0]);
     }
 }
