3158af2c3d3217445fa93a32fbf0b4e3bd9182fd
Jakubwlodarczyk <Kubawl.91@gmail.com>
2017-06-14 12:04:27 +0200

    Add methods to check if Seed value is Empty, and setCellContent

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -2,6 +2,7 @@ package controller;
 
 import model.Game;
 import model.GameState;
+import model.Seed;
 import view.DisplayGame;
 
 public class GameController {
@@ -12,19 +13,25 @@ public class GameController {
         DisplayGame.clearScreen();
         DisplayGame.printWelcomeInfo(game.getCurrentPlayer());
 
-
         while (game.getCurrentState().equals(GameState.PLAYING)) {
             DisplayGame.displayBoard(game.getBoard());
             Integer row = DisplayGame.userInput(game.getCurrentPlayer(), "enter your move (row[1-3]): ");
             Integer col = DisplayGame.userInput(game.getCurrentPlayer(), "enter your move (col[1-3]): ");
-            game.getBoard().getCells()[row-1][col-1].setContent(game.getCurrentPlayer());
+            if (checkIfSeedIsEmpty(row, col)) {
+                setCellContent(row,col,game.getCurrentPlayer());
+            }
             game.getBoard().hasWon();
             game.getBoard().isDraw();
             game.togglePlayer();
             DisplayGame.clearScreen();
+            }
         }
+
+    private boolean checkIfSeedIsEmpty(int row, int col) {
+        return game.getBoard().getCells()[row-1][col-1].getContent().equals(Seed.EMPTY);
     }
 
-
-
+    private void setCellContent(int row, int col, Seed content) {
+        game.getBoard().getCells()[row - 1][col - 1].setContent(content);
+    }
 }
