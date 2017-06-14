bb58e262a7dcba33776b6eb70e7d1cd15109841e
Jakubwlodarczyk <Kubawl.91@gmail.com>
2017-06-14 09:41:23 +0200

    Change the 'magic numbers' to local variables

--- src/test/java/model/BoardTest.java
+++ src/test/java/model/BoardTest.java
@@ -8,75 +8,83 @@ import static org.junit.jupiter.api.Assertions.*;
 class BoardTest {
     private Board board;
     private Cell[][] cells;
+    private int row1 = 0;
+    private int row2 = 1;
+    private int row3 = 2;
+    private int col1 = 0;
+    private int col2 = 1;
+    private int col3 = 2;
 
     @BeforeEach
     void setup() {
         board = new Board();
-        cells = new Cell[3][3];
-        cells[0][0] = new Cell(1,1);
-        cells[0][1] = new Cell(1,2);
-        cells[0][2] = new Cell(1,3);
-        cells[1][0] = new Cell(2,1);
-        cells[1][1] = new Cell(2,2);
-        cells[1][2] = new Cell(2,3);
-        cells[2][0] = new Cell(3,1);
-        cells[2][1] = new Cell(3,2);
-        cells[2][2] = new Cell(3,3);
+        int numberOfRows = 3;
+        int numberOfCols = 3;
+        cells = new Cell[numberOfRows][numberOfCols];
+        cells[row1][col1] = new Cell(1,1);
+        cells[row1][col2] = new Cell(1,2);
+        cells[row1][col3] = new Cell(1,3);
+        cells[row2][col1] = new Cell(2,1);
+        cells[row2][col2] = new Cell(2,2);
+        cells[row2][col3] = new Cell(2,3);
+        cells[row3][col1] = new Cell(3,1);
+        cells[row3][col2] = new Cell(3,2);
+        cells[row3][col3] = new Cell(3,3);
         board.setCells(cells);
     }
 
     @Test
     void testHasWonMethodReturnTrueWhenSeedsInRowAreSame() {
-        board.getCells()[0][0].setContent(Seed.CROSS);
-        board.getCells()[0][1].setContent(Seed.CROSS);
-        board.getCells()[0][2].setContent(Seed.CROSS);
+        board.getCells()[row1][col1].setContent(Seed.CROSS);
+        board.getCells()[row2][col2].setContent(Seed.CROSS);
+        board.getCells()[row3][col3].setContent(Seed.CROSS);
         assertEquals(true, board.hasWon());
     }
 
     @Test
     void testHasWonMethodReturnTrueWhenSeedsInColAreSame() {
-        board.getCells()[0][0].setContent(Seed.CROSS);
-        board.getCells()[1][0].setContent(Seed.CROSS);
-        board.getCells()[2][0].setContent(Seed.CROSS);
+        board.getCells()[row1][col1].setContent(Seed.CROSS);
+        board.getCells()[row2][col1].setContent(Seed.CROSS);
+        board.getCells()[row3][col1].setContent(Seed.CROSS);
         assertEquals(true, board.hasWon());
     }
 
     @Test
     void testHasWonMethodReturnTrueWhenSeedsOnLeftBevelAreSame() {
-        board.getCells()[0][0].setContent(Seed.CROSS);
-        board.getCells()[1][1].setContent(Seed.CROSS);
-        board.getCells()[2][2].setContent(Seed.CROSS);
+        board.getCells()[row1][col1].setContent(Seed.CROSS);
+        board.getCells()[row2][col2].setContent(Seed.CROSS);
+        board.getCells()[row3][col3].setContent(Seed.CROSS);
         assertEquals(true, board.hasWon());
     }
 
     @Test
     void testHasWonMethodReturnTrueWhenSeedsOnRightBevelAreSame() {
-        board.getCells()[2][2].setContent(Seed.CROSS);
-        board.getCells()[1][1].setContent(Seed.CROSS);
-        board.getCells()[0][0].setContent(Seed.CROSS);
+        board.getCells()[row3][col3].setContent(Seed.CROSS);
+        board.getCells()[row2][col2].setContent(Seed.CROSS);
+        board.getCells()[row1][col1].setContent(Seed.CROSS);
         assertEquals(true, board.hasWon());
     }
 
     @Test
     void testIsDrawMethodReturnTrueWhenAllSeedInCellArrayAreDifferentFromEmptyAndNotSameInLine() {
-        board.getCells()[0][0].setContent(Seed.CROSS);
-        board.getCells()[0][1].setContent(Seed.NOUGHT);
-        board.getCells()[0][2].setContent(Seed.CROSS);
-        board.getCells()[1][0].setContent(Seed.NOUGHT);
-        board.getCells()[1][1].setContent(Seed.CROSS);
-        board.getCells()[1][2].setContent(Seed.NOUGHT);
-        board.getCells()[2][0].setContent(Seed.NOUGHT);
-        board.getCells()[2][1].setContent(Seed.CROSS);
-        board.getCells()[2][2].setContent(Seed.NOUGHT);
+        board.getCells()[row1][col1].setContent(Seed.CROSS);
+        board.getCells()[row1][col2].setContent(Seed.NOUGHT);
+        board.getCells()[row1][col3].setContent(Seed.CROSS);
+        board.getCells()[row2][col1].setContent(Seed.NOUGHT);
+        board.getCells()[row2][col2].setContent(Seed.CROSS);
+        board.getCells()[row2][col3].setContent(Seed.NOUGHT);
+        board.getCells()[row3][col1].setContent(Seed.NOUGHT);
+        board.getCells()[row3][col2].setContent(Seed.CROSS);
+        board.getCells()[row3][col3].setContent(Seed.NOUGHT);
         assertEquals(true, board.isDraw());
     }
 
     @Test
     void testGetCellsMethodReturnValidCellsValue() {
         for (Cell[] cells: board.getCells()){
-            assertEquals(Seed.EMPTY, cells[0].getContent());
-            assertEquals(Seed.EMPTY, cells[1].getContent());
-            assertEquals(Seed.EMPTY, cells[2].getContent());
+            assertEquals(Seed.EMPTY, cells[row1].getContent());
+            assertEquals(Seed.EMPTY, cells[row2].getContent());
+            assertEquals(Seed.EMPTY, cells[row3].getContent());
         }
     }
 }
