c03a508841874aa4d78569c34b3f2e998925e3e2
Jakubwlodarczyk <Kubawl.91@gmail.com>
2017-06-15 21:25:31 +0200

    Add possibility to play game again (add another while loop)

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -3,9 +3,12 @@ package controller;
 import model.Game;
 import model.GameState;
 import model.Seed;
+import ui.UserInput;
 import view.DisplayGame;
 
+
 public class GameController {
+
     private Game game = new Game();
 
     public void initGame() {
@@ -15,36 +18,48 @@ public class GameController {
     }
 
     public void startGame() {
-        while (game.getCurrentState().equals(GameState.PLAYING)) {
-            DisplayGame.displayBoard(game.getBoard());
-            Integer row = DisplayGame.getPositiveIntInput(game.getCurrentPlayer(), "enter your move (row[1-3]): ");
-            Integer col = DisplayGame.getPositiveIntInput(game.getCurrentPlayer(), "enter your move (col[1-3]): ");
-            if (checkIfSeedIsEmpty(row, col)) {
-                setCellContent(row, col, game.getCurrentPlayer());
-            } else {
-                DisplayGame.displayMessage();
-                continue;
-            }
-            if (game.getBoard().hasWon()) {
-                if (game.getCurrentPlayer().equals(Seed.CROSS)) {
-                    game.setCurrentState(GameState.CROSS_WON);
+        boolean playGame = true;
+        while (playGame) {
+            while (game.getCurrentState().equals(GameState.PLAYING)) {
+                DisplayGame.displayBoard(game.getBoard());
+                Integer row = UserInput.getPositiveIntInput(game.getCurrentPlayer(), "enter your move (row[1-3]): ");
+                Integer col = UserInput.getPositiveIntInput(game.getCurrentPlayer(), "enter your move (col[1-3]): ");
+                if (isSeedEmpty(row, col)) {
+                    setCellContent(row, col, game.getCurrentPlayer());
                 } else {
-                    game.setCurrentState(GameState.NOUGHT_WON);
+                    DisplayGame.displayMessage("Chose another place sir!");
+                    continue;
                 }
-                break;
+                if (game.getBoard().hasWon()) {
+                    if (game.getCurrentPlayer().equals(Seed.CROSS)) {
+                        game.setCurrentState(GameState.CROSS_WON);
+                    } else {
+                        game.setCurrentState(GameState.NOUGHT_WON);
+                    }
+                    break;
+                }
+                if (game.getBoard().isDraw()) {
+                    game.setCurrentPlayer(Seed.EMPTY);
+                    game.setCurrentState(GameState.DRAW);
+                    break;
+                }
+                game.switchPlayer();
+                DisplayGame.clearScreen();
             }
-            if (game.getBoard().isDraw()) {
-                game.setCurrentPlayer(Seed.EMPTY);
-                game.setCurrentState(GameState.DRAW);
-                break;
+            DisplayGame.displayGameResult(game.getCurrentPlayer());
+            DisplayGame.displayMessage("\n");
+            if (UserInput.getUserPlayAgainAnswer().equals("n")) {
+                playGame = false;
+                DisplayGame.displayMessage("Hope you enjoyed! C U later!");
+            } else {
+                game.initGame();
+                DisplayGame.clearScreen();
+                DisplayGame.printWelcomeInfo(game.getCurrentPlayer());
             }
-            game.togglePlayer();
-            DisplayGame.clearScreen();
-            }
-        DisplayGame.displayGameResultOfGame(game.getCurrentPlayer());
         }
+    }
 
-    private boolean checkIfSeedIsEmpty(int row, int col) {
+    boolean isSeedEmpty(int row, int col) {
         return game.getBoard().getCells()[row-1][col-1].getContent().equals(Seed.EMPTY);
     }
 
