14ed2e78ca463fa546d5cfde072a0594b6aca5bc
Jakubwlodarczyk <Kubawl.91@gmail.com>
2017-06-15 21:26:30 +0200

    change Board class attributes to private, and delete unused attributes

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -3,15 +3,12 @@ package model;
 public class Board {
 
     private Cell[][] cells;
-    int row1 = 0;
-    int row2 = 1;
-    int row3 = 2;
-    int col1 = 0;
-    int col2 = 1;
-    int col3 = 2;
-    int firstColumn = 0;
-    int secondColumn = 1;
-    int thirdColumn = 2;
+    private int row1 = 0;
+    private int row2 = 1;
+    private int row3 = 2;
+    private int col1 = 0;
+    private int col2 = 1;
+    private int col3 = 2;
 
     Board() {
         this.cells = new Cell[3][3];
@@ -52,12 +49,12 @@ public class Board {
     private boolean seedsInColAreSame() {
         int rowIndex = 0;
         for (Cell[] cell : cells) {
-            if (cells[firstColumn][rowIndex].getContent().equals(Seed.CROSS) &&
-                    cells[secondColumn][rowIndex].getContent().equals(Seed.CROSS) &&
-                    cells[thirdColumn][rowIndex].getContent().equals(Seed.CROSS) ||
-                    cells[firstColumn][rowIndex].getContent().equals(Seed.NOUGHT) &&
-                            cells[secondColumn][rowIndex].getContent().equals(Seed.NOUGHT) &&
-                            cells[thirdColumn][rowIndex].getContent().equals(Seed.NOUGHT)) {
+            if (cells[col1][rowIndex].getContent().equals(Seed.CROSS) &&
+                    cells[col2][rowIndex].getContent().equals(Seed.CROSS) &&
+                    cells[col3][rowIndex].getContent().equals(Seed.CROSS) ||
+                    cells[col1][rowIndex].getContent().equals(Seed.NOUGHT) &&
+                            cells[col2][rowIndex].getContent().equals(Seed.NOUGHT) &&
+                            cells[col3][rowIndex].getContent().equals(Seed.NOUGHT)) {
                 return true;
             }
             rowIndex++;
@@ -67,12 +64,12 @@ public class Board {
 
     private boolean seedsInRowAreSame() {
         for (Cell[] row: this.cells) {
-            if (row[firstColumn].getContent().equals(Seed.CROSS) &&
-                    row[secondColumn].getContent().equals(Seed.CROSS) &&
-                    row[thirdColumn].getContent().equals(Seed.CROSS) ||
-                    row[firstColumn].getContent().equals(Seed.NOUGHT) &&
-                            row[secondColumn].getContent().equals(Seed.NOUGHT) &&
-                            row[thirdColumn].getContent().equals(Seed.NOUGHT)){
+            if (row[col1].getContent().equals(Seed.CROSS) &&
+                    row[col2].getContent().equals(Seed.CROSS) &&
+                    row[col3].getContent().equals(Seed.CROSS) ||
+                    row[col1].getContent().equals(Seed.NOUGHT) &&
+                            row[col2].getContent().equals(Seed.NOUGHT) &&
+                            row[col3].getContent().equals(Seed.NOUGHT)){
                 return true;
             }
         }
